311d8a9e684e917162b42eeafe03068c4b745fba
iroha999 <135195863+iroha999@users.noreply.github.com>
2025-06-04 19:39:50 +0900

    fix: compile error

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "portfolio"
-  description: "portfolio site
+  description: "portfolio site"
     generator: 'v0.dev'
 }
 
